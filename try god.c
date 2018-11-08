e8c544524b02c0e155ee676707e6b70389f6644e
Foolsh Kwong (ipad) <sfacs28013376666@gmail.com>
2018-11-08 16:44:21 +0800

    GG
    Hh

--- try god.c
+++ try god.c
@@ -1,13 +1,15 @@
 #import <stdio.h>
+extern void utility();
 
 int main() {
 	int God = 20;
 	int Foolsh = 1;
 	printf("If god is %d\n", God);
+	utility();
 }
 
 void utility() {
-
-
-
+	  float i_am_God, i_am_on9; 
+	  i_am_God = camen;
+	  printf("Jesus is here %d \n", i_am_God);
 }
